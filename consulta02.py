49b1b20ba17ff128b927c9b19404789c483633cf
ikestrella <ikestrella@utpl.edu.ec>
2025-05-27 09:11:00 -0500

    TallerClase

--- consulta02.py
+++ consulta02.py
@@ -16,7 +16,9 @@ session = Session()
 # Todos los departamentos que tengan notas de entregas menores o iguales a 0.3 . 
 # En función de los departamentos, presentar el nombre del departamento y el número de cursos que tiene cada departamento
 
+# Se hace una query haciendo un Join con curso despues a tarea y al final a entrega para poder obtener las calificaciones menor a 0.3
 departamentos = session.query(Departamento).join(Curso).join(Tarea).join(Entrega).filter(Entrega.calificacion<=0.3).all()
 
+# Se presenta lo obtenido de la consulta
 for d in departamentos:
     print(f"Departamento: {d.nombre} - Numero Cursos: {len(d.cursos)}")
