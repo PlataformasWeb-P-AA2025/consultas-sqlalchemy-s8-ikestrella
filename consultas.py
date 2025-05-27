246f56e42d18b87f1e1be747dbdf16c4d019a6b9
ikestrella <ikestrella@utpl.edu.ec>
2025-05-27 08:52:49 -0500

    TallerClase

--- consultas.py
+++ consultas.py
@@ -19,10 +19,12 @@ session = Session()
 # Se hace una Querty obteniendo Entrega, Tarea, Estudiante, Instructor, Departamento haciendo joins desde Entrega hasta Tarea Donde esta el curso 
 # y a partir de el se puede hacer join con Curso y desde Curso hacer Join hasta Instructor para el nombre y Join hasta departamento para poder 
 # hacer el filter Para Arte, y join desde Entrega hasta Estudiante para el nombre del estudiante 
-entregas = session.query(Entrega, Tarea, Estudiante, Instructor, Departamento).join(Tarea).join(Estudiante).join(Curso).join(Instructor).join(Departamento).filter(Departamento.nombre=='Arte')
+entregas = session.query(Entrega).join(Tarea).join(Estudiante).join(Curso).join(Instructor).join(Departamento).filter(Departamento.nombre=='Arte')
 
 
 # Se muestra lo obtenido de la consulta
 for e in entregas:
-    print(f"Nombre Tarea: {e[1].titulo} - Nombre Estudiante: {e[2].nombre} - Calificacion: {e[0].calificacion} - Nombre Instructor: {e[3].nombre} - Departamento: {e[4].nombre}")
+    print(f"Nombre Tarea: {e.tarea.titulo} - Nombre Estudiante: {e.estudiante.nombre} - Calificacion: {e.calificacion} - Nombre Instructor: {e.tarea.curso.instructor.nombre} - Nombre Curso: {e.tarea.curso.departamento.nombre}")
     print()
+
+# - Calificacion: {e[0].calificacion} - Nombre Instructor: {e[3].nombre} - Departamento: {e[4].nombre}
